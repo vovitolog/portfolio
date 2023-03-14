3ea42a71e038cc359541f1366c480597d9b38630
vovitolog <vovitolog@gmail.com>
2023-03-14 21:50:34 +0300

    feat: add styles to About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import classes from "./About.module.css";
 import styleContainer from "../../common/styles/Container.module.css"
 import photo from "../../assets/images/vladimir.jpg"
+import {getCLS} from "web-vitals";
 
 export const About = () => {
     return <section className={classes.about}>
-        <div className={styleContainer.container}>
-            <div className={classes.text}>
-                <span>Hi There</span>
-                <h2>I'm Vladimir Rodionov</h2>
-                <p>Frontend Developer</p>
-                <p>You can find more information about me in my CV</p>
+        <div className={classes.aboutContainer}>
+            <div className={classes.person}>
+                <div className={classes.personText}>
+                    <span>Hi There</span>
+                    <h2 className={classes.title}>I'm Vladimir Rodionov</h2>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={classes.personContact}>
+                    Download my curriculum vitae:
+                    <p>Frontend Developer</p>
+                    <p>Frontend Developer</p>
+                    <p>Frontend Developer</p>
+                </div>
             </div>
-            <div className={classes.photo}>
+            <div className={classes.photoWrapper}>
                 <img className={classes.photo} src={photo} alt="Фото Владимир"/>
             </div>
         </div>
