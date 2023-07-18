704f70bddb58755bdb9f0ae49fbe5a69a1e949a7
vovitolog <vovitolog@gmail.com>
2023-07-18 22:58:29 +0300

    Initial commit

--- src/components/header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import classes from "./Header.module.css";
-import {Nav} from "../nav/Nav";
-
-export const Header = () => {
-    return <div className={classes.header}>
-                <Nav/>
-    </div>
-}
--- src/components/projects/project/Project.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import classes from "./Project.module.css";
-
-export const Project = (props) => {
-    return <div className={classes.project}>
-        <img src={props.img}/>
-        {props.name}
-    </div>
-}
